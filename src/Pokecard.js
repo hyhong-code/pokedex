c3fca5f4d4272f225a705e64b9aef0444fb3c00f
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-01 15:32:47 -0700

    add Pokegame component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
-import { transformId } from "./helper";
 const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
 class Pokecard extends Component {
@@ -8,7 +7,7 @@ class Pokecard extends Component {
     const {
       data: { id, name, type, base_experience },
     } = this.props;
-    const transformedId = transformId(id);
+    const transformedId = id < 100 ? `00${id}`.slice(-3) : id;
     const imgSrc = `${POKE_API}${transformedId}.png`;
     return (
       <div className="Pokecard">
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,10 +1,37 @@
-// import React, { Component } from "react";
-// import Pokecard from "./Pokecard";
+import React, { Component } from "react";
+import Pokedex from "./Pokedex";
 
-// class Pokegame extends Component {
-//   render() {
-//     return
-//   }
-// }
+class Pokegame extends Component {
+  static defaultProps = {
+    pokemons: [
+      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+    ],
+  };
 
-// export default Pokegame;
+  render() {
+    let hand1 = [];
+    let hand2 = [...this.props.pokemons];
+    while (hand2.length > hand1.length) {
+      const idx = Math.floor(Math.random() * hand2.length);
+      const [removed] = hand2.splice(idx, 1);
+      hand1.push(removed);
+    }
+
+    return (
+      <div>
+        <h1>Pokegame!</h1>
+        <Pokedex pokemons={hand1} />
+        <Pokedex pokemons={hand2} />
+      </div>
+    );
+  }
+}
+
+export default Pokegame;
